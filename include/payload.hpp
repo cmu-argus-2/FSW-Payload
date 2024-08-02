01f4db80440258906dbf99a65a21da1c97dc2f02
ibrahima <ibrahima.sr.sow@gmail.com>
2024-08-02 12:31:56 -0400

    add payload states enum

--- include/payload.hpp
+++ include/payload.hpp
@@ -1,12 +1,34 @@
 #ifndef PAYLOAD_HPP
 #define PAYLOAD_HPP
 
+#include "queues.hpp"
+
+enum class PayloadState : uint8_t {
+    STARTUP = 0x00,
+    IDLE = 0x01,
+    NOMINAL = 0x02,
+    SAFE_MODE = 0x03
+};
+
+// Function to convert PayloadState enum to string
+const char* ToString(PayloadState state);
+
 class Payload
 {
 public:
     Payload();
     // ~Payload();
     void run();
+
+
+
+private:
+
+    PayloadState state;
+    RX_Queue rx_queue;
+    TX_Queue tx_queue;
+    
+    
 };
 
 #endif // PAYLOAD_HPP
--- src/payload.cpp
+++ src/payload.cpp
@@ -1,8 +1,21 @@
 #include "payload.hpp"
 #include <iostream>
 
+const char* ToString(PayloadState state) {
+    switch (state) {
+        case PayloadState::STARTUP: return "STARTUP";
+        case PayloadState::NOMINAL: return "NOMINAL";
+        case PayloadState::IDLE: return "IDLE";
+        case PayloadState::SAFE_MODE: return "SAFE_MODE";
+        default: return "UNKNOWN";
+    }
+}
+
+
 
 Payload::Payload()
+:
+state(PayloadState::STARTUP)
 {
     // Constructor
 }

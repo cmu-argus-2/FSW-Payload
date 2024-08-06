5524d46e11723ea44a6e8f1d49c039c1ac583ab5
ibrahima <ibrahima.sr.sow@gmail.com>
2024-08-06 11:22:58 -0400

    .

---
+++ tests/test_commands.cpp
@@ -0,0 +1,10 @@
+/*
+TODO
+- Check total size of commands array
+- Check uniqueness of command IDs
+- check if all commands are declared 
+- check contguity of command IDs (with total size)
+
+
+
+*/

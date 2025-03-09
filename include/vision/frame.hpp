6b6e2b75f43eb6689d97bfe1df3b2fa6a94256ee
ibrahima <ibrahima.sr.sow@gmail.com>
2025-03-09 01:25:57 -0800

    added blur detection + frame copy constructors due to locks

--- include/vision/frame.hpp
+++ include/vision/frame.hpp
@@ -1,17 +1,23 @@
 #ifndef FRAME_HPP
 #define FRAME_HPP
 
-#include "spdlog/spdlog.h"
 #include <cstdint>
-#include <opencv2/opencv.hpp>
 #include <vector>
+#include <mutex>
+#include <memory>
+
+#include <opencv2/opencv.hpp>
+
 #include <vision/rc.hpp>
 #include <vision/ld.hpp>
 
 // 4608×2592
-#define DEFAULT_FRAME_WIDTH 4608
-#define DEFAULT_FRAME_HEIGHT 2592
-#define EARTH_THRESHOLD 0.7
+inline constexpr int DEFAULT_FRAME_WIDTH = 4608;
+inline constexpr int DEFAULT_FRAME_HEIGHT = 2592;
+inline constexpr uint64_t FULL_RES_PIXEL_SIZE = DEFAULT_FRAME_WIDTH * DEFAULT_FRAME_HEIGHT;
+
+inline constexpr double EARTH_THRESHOLD = 0.7;
+inline constexpr double BLUR_THRESHOLD = 50.0;
 
 
 class Frame 
@@ -20,6 +26,9 @@ public:
     Frame();
     Frame(int cam_id, const cv::Mat& img, std::uint64_t timestamp);
     Frame(int cam_id, cv::Mat&& img, std::uint64_t timestamp);
+    Frame(const Frame& other);
+
+    Frame& operator=(const Frame& other);
 
     void Update(int cam_id, const cv::Mat& img, std::uint64_t timestamp);
     
@@ -35,6 +44,8 @@ public:
     void AddLandmark(float x, float y, uint16_t class_id, RegionID region_id);
     void ClearLandmarks();
 
+    bool IsBlurred();
+
 private:
     int _cam_id;
     cv::Mat _img;
@@ -42,6 +53,9 @@ private:
 
     std::vector<RegionID> _region_ids;  // Container for regions
     std::vector<Landmark> _landmarks;  // Container for landmarks
+
+    // mutex is not copyable
+    std::shared_ptr<std::mutex> _img_mtx; // using shared_ptr for copyability (private anyway)
 };
 
 
--- src/vision/frame.cpp
+++ src/vision/frame.cpp
@@ -1,18 +1,21 @@
 #include "spdlog/spdlog.h"
 #include "vision/frame.hpp"
+#include <opencv2/opencv.hpp>
 
 Frame::Frame()  
 :
 _cam_id(-1),
 _img(DEFAULT_FRAME_WIDTH, DEFAULT_FRAME_HEIGHT, CV_8UC3),
-_timestamp(0)
+_timestamp(0),
+_img_mtx(std::make_shared<std::mutex>())
 {}
 
 
 Frame::Frame(int cam_id, const cv::Mat& img, std::uint64_t timestamp)
     : _cam_id(cam_id),
       _img(img),  
-      _timestamp(timestamp)
+      _timestamp(timestamp),
+      _img_mtx(std::make_shared<std::mutex>())
 {}
 
 // For rvalue reference
@@ -20,9 +23,30 @@ Frame::Frame(int cam_id, cv::Mat&& img, std::uint64_t timestamp)
 :
 _cam_id(cam_id),
 _img(std::move(img)),
-_timestamp(timestamp)
+_timestamp(timestamp),
+_img_mtx(std::make_shared<std::mutex>())
 {}
 
+Frame::Frame(const Frame& other)
+    : _cam_id(other._cam_id),
+      _img(other._img.clone()), 
+      _timestamp(other._timestamp),
+      _img_mtx(std::make_shared<std::mutex>())  // creating a new mutex per instance to avoid intercopy locking
+{}
+
+
+Frame& Frame::operator=(const Frame& other)
+{
+    if (this != &other) {
+        _img = other._img.clone(); 
+        _cam_id = other._cam_id;
+        _timestamp = other._timestamp;
+        _region_ids = other._region_ids;
+        _landmarks = other._landmarks;
+        _img_mtx = std::make_shared<std::mutex>(); // new mutex per instance
+    }
+    return *this;
+}
 
 void Frame::Update(int cam_id, const cv::Mat& img, std::uint64_t timestamp) 
 {
@@ -74,4 +98,36 @@ void Frame::AddLandmark(float x, float y, uint16_t class_id, RegionID region_id)
 void Frame::ClearLandmarks()
 {
     _landmarks.clear();
+}
+
+bool Frame::IsBlurred()
+{
+    // the more an image is blurred, the less edges there is 
+    // need to lock in there, since the buffer is shared 
+
+    static std::vector<uint8_t> grey_buffer(FULL_RES_PIXEL_SIZE);
+    static std::vector<double> laplacian_buffer(FULL_RES_PIXEL_SIZE);
+
+    cv::Scalar mean, std_dev;
+    {   
+        std::lock_guard<std::mutex> lck(*_img_mtx);
+        cv::Size size = _img.size(); 
+        cv::Mat gray(size, CV_8U, grey_buffer.data());
+        cv::Mat laplacian(size, CV_64F, laplacian_buffer.data());
+
+        cv::cvtColor(_img, gray, cv::COLOR_BGR2GRAY);
+        cv::Laplacian(gray, laplacian, CV_64F);
+
+        cv::meanStdDev(laplacian, mean, std_dev);
+    }
+
+    double variance = std_dev[0] * std_dev[0];  // variance = sigma^2
+
+    bool is_blurred = variance < BLUR_THRESHOLD;
+
+    SPDLOG_INFO("Variance is {}", variance);
+    SPDLOG_INFO("Frame is{}blurred", is_blurred ? " " : " not ");
+ 
+    return is_blurred;
+
 }

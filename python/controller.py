b33ac75e29dbd0a46c5b3c488b7fa9f3ac7a462c
ibrahima <ibrahima.sr.sow@gmail.com>
2025-03-13 02:34:37 -0700

    adding python interface backbone

---
+++ python/controller.py
@@ -0,0 +1,11 @@
+# Payload Control Interface
+
+class PayloadState: # From the host perspective
+    pass
+
+class PayloadTM: # Simple data structure holder
+    pass
+
+class PayloadHostControl:
+    pass
+

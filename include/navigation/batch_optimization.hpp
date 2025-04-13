86d7c3f04d8b522d568284c9fea19b151a6ea39b
ibrahima <ibrahima.sr.sow@gmail.com>
2025-04-12 23:32:36 -0700

    add progress enum batchopt

--- include/navigation/batch_optimization.hpp
+++ include/navigation/batch_optimization.hpp
@@ -1,10 +1,12 @@
 #ifndef BATCH_OPTIMIZATION_HPP
 #define BATCH_OPTIMIZATION_HPP
 
-
-
-
-
-
+enum class BatchOptimizationState
+{
+    NOT_STARTED = 0,
+    RUNNING = 1,
+    COMPLETED = 2,
+    FAILED = 3
+};
 
 #endif // BATCH_OPTIMIZATION_HPP

e6010421825210aebc9e111e794dbe8506c639bd
ibrahima <ibrahima.sr.sow@gmail.com>
2025-07-06 01:40:22 -0700

    rework regions ID, compile-time mappings, and clean-ups

--- include/vision/ld.hpp
+++ include/vision/ld.hpp
@@ -2,14 +2,15 @@
 #define LD_HPP
 
 #include <cstdint>
+#include "vision/regions.hpp"
 
 struct Landmark 
 {
     float x, y;         // Centroid position
     uint16_t class_id;  // Class ID
-    uint8_t region_id; // Encoded MGRS region
+    RegionID region_id; // Encoded MGRS region
 
-    Landmark(float x_, float y_, uint16_t class_id_, uint8_t region_id_)
+    Landmark(float x_, float y_, uint16_t class_id_, RegionID region_id_)
         : x(x_), y(y_), class_id(class_id_), region_id(region_id_) {}
 };
 

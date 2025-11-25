d7fff7adf92f7968b2d8c2937dae56c7dd8c8e1c
Sophia <owlsandmoreowls@gmail.com>
2025-11-24 23:02:51 -0500

    added camera test code, not working due to cmake errors

---
+++ tests/test_camera.cpp
@@ -0,0 +1,37 @@
+#include <memory>
+#include "spdlog/spdlog.h"
+#include "vision/camera_manager.hpp"
+#include "configuration.hpp"
+#include "core/data_handling.hpp"
+
+int main(int argc, char** argv)
+{
+    spdlog::info("Initializing camera test");
+
+    auto config = std::make_unique<Configuration>();
+    if (!config->LoadConfig())
+    {
+        spdlog::error("Failed to load configuration.");
+        return 1;
+    }
+
+    const auto& cam_configs = config->GetCameraConfigs();
+    CameraManager cam_manager(cam_configs);
+
+    spdlog::info("Enabling cameras...");
+    std::array<bool, NUM_CAMERAS> activated;
+    int count = cam_manager.EnableCameras(activated);
+    spdlog::info("Cameras enabled: {}", count);
+
+    spdlog::info("Capturing frames");
+    cam_manager.CaptureFrames();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+    spdlog::info("Saving latest frames...");
+    uint8_t saved = cam_manager.SaveLatestFrames();
+
+    spdlog::info("Saved {} frame(s).", saved);
+
+    return 0;
+}

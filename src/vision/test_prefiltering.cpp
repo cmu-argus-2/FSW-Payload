26ae8ea93a8e451ef224ac7dc7691927acd3594e
prchall123 <prcachim@gmail.com>
2025-11-26 16:18:08 -0500

    prefiltering function added

---
+++ src/vision/test_prefiltering.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "vision/prefiltering.h"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Usage: ./prefilter <path_to_image>" << std::endl;
+        return -1;
+    }
+
+    std::string image_path = argv[1];
+    std::cout << "Processing: " << image_path << std::endl;
+
+    PrefilterResult res = prefilter_image(image_path);
+
+    if (!res.error.empty()) {
+        std::cerr << "Error: " << res.error << std::endl;
+        return -1;
+    }
+
+    std::cout << "--- Results ---" << std::endl;
+    std::cout << "Passed: " << (res.passed ? "Yes" : "No") << std::endl;
+    std::cout << "Dominant Type: " << res.dominant_type << std::endl;
+    std::cout << "Cloudiness: " << res.cloudiness << "%" << std::endl;
+    std::cout << "Contrast Std Dev: " << res.contrast_std << std::endl;
+    std::cout << "Color Std Dev: " << res.color_std << std::endl;
+    std::cout << "Avg RGB: (" << res.avg_color_rgb[0] << ", " 
+              << res.avg_color_rgb[1] << ", " << res.avg_color_rgb[2] << ")" << std::endl;
+
+    return 0;
+}

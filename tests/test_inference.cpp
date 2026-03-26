303981fafb45eb3f48473e72eba15394e7383488
prchall123 <prcachim@gmail.com>
2026-03-26 17:22:06 +0000

    moving scripts to script folder

---
+++ tests/test_inference.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "spdlog/spdlog.h"
+// #include <imu/imu_manager.hpp>
+
+// Test orchestrator class configuration
+TEST(OrchestratorTest, NNetConfiguration) 
+{
+    // TODO
+}
+
+// Test inference with training data
+TEST(OrchestratorTest, TrainingDataTest) 
+{
+    // TODO
+}
